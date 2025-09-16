3aa14bcdc711bc03ec154a2079ef21e6b0b41512
moa2-assis <moa2@cesar.org.br>
2025-09-16 11:12:21 -0300

    minha feature 5 e fix da 3

--- main.py
+++ main.py
@@ -7,3 +7,5 @@ print("Feature 2 agora tá lindamente d+ bom d+")
 print("Feature 3 tá funcionandoooo - agora real oficial pois não tava funcionando")
 
 print("Feature 4 feita agorinha agorinha")
+
+print("Feature 5 cheia de gambiarra mas funcional")
