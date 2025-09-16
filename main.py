6faa98020b87583b496cd34f0d430861b051aa60
moa2-assis <moa2@cesar.org.br>
2025-09-16 11:10:14 -0300

    update da feature 3

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ print("Feature 1 completamente implementada ficou LINDO")
 
 print("Feature 2 agora tá lindamente d+ bom d+")
 
-print("Feature 3 tá funcionandoooo")
+print("Feature 3 tá funcionandoooo - agora real oficial pois não tava funcionando")
 
 print("Feature 4 feita agorinha agorinha")
