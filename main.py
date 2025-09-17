3560ff01b9588640f81e6bbe546112d6cb7e7ec5
moa2-assis <moa2@cesar.org.br>
2025-09-17 08:13:35 -0300

    feature 6 feita no vscode

--- main.py
+++ main.py
@@ -9,3 +9,5 @@ print("Feature 3 tá funcionandoooo - agora real oficial pois não tava funciona
 print("Feature 4 feita agorinha agorinha")
 
 print("Feature 5 cheia de gambiarra mas funcional")
+
+print("Feature 6 feita no vscode")
